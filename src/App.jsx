9afc591179c7a886e9d7f9774ed1426b30d9a53c
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-12 08:53:07 -0300

    Arrumando a API selecionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const App = () => {
   const [amout, setAmout] = useState(1)
+  const [from, setFrom] = useState('USD')
+  const [to, setTo] = useState('BRL')
+  const [exchange, setExchange] = useState(null)
 
-  const changeAmout = e => setAmout(prev => prev === '' ? 0 : +e.target.value)
+  useEffect(() => {
+    fetch(`https://economia.awesomeapi.com.br/last/${from}-${to}`)
+      .then(res => res.json())
+      .then(data => setExchange(data))
+      .catch(console.log)
+  }, [exchange])
+
+  const handleChangeFrom = e => setFrom(e.target.value)
+  const handleChangeTo = e => setTo(e.target.value)
+  const handleChangeAmout = e => setAmout(prev => prev === '' ? 0 : +e.target.value)
 
   return (
     <>
-      <input value={amout} type="number" autoFocus onChange={changeAmout} />
+      <input value={amout} type="number" autoFocus onChange={handleChangeAmout} />
 
       <div className="selects">
-        <select>
+        <select value={from} onChange={handleChangeFrom}>
           <option value="BRL">BRL</option>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
         </select>
-        <select>
+        <select value={to} onChange={handleChangeTo}>
           <option value="BRL">BRL</option>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
