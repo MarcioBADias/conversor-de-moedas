829073998ad0c3af72a0ee8e28beafc84dc9c04c
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-12 08:41:49 -0300

    MRP ⚠

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-const App = () => (
-  <>
-    <input type="number" autoFocus />
+import { useState } from "react"
 
-    <div className="selects">
-      <select>
-        <option value="BRL">BRL</option>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-      </select>
-      <select>
-        <option value="BRL">BRL</option>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-      </select>
-    </div>
+const App = () => {
+  const [amout, setAmout] = useState(1)
 
-    <h2>4.87 BRL</h2>
-  </>
-)
+  const changeAmout = e => setAmout(prev => prev === '' ? 0 : +e.target.value)
+
+  return (
+    <>
+      <input value={amout} type="number" autoFocus onChange={changeAmout} />
+
+      <div className="selects">
+        <select>
+          <option value="BRL">BRL</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
+        <select>
+          <option value="BRL">BRL</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
+      </div>
+
+      <h2>4.87 BRL</h2>
+    </>
+  )
+}
 
 export { App }
