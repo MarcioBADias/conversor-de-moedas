b6c555d7d0bb3006a0d72ff4029b87ea8adbe52f
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-15 09:33:56 -0300

    Implementando os efeitos colaterais

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,39 @@ const App = () => {
   const [from, setFrom] = useState('USD')
   const [to, setTo] = useState('BRL')
   const [exchange, setExchange] = useState(null)
+  const [loading, setLoading] = useState(null)
 
   useEffect(() => {
-    fetch(`https://economia.awesomeapi.com.br/last/${from}-${to}`)
-      .then(res => res.json())
-      .then(data => setExchange(data))
-      .catch(console.log)
-  }, [exchange])
+    if (exchange === null) {
+      return
+    }
+
+    document.title = `${exchange.ask} ${to}`
+    return () => document.title = 'Conversor de moedas'
+  }, [exchange, to])
+
+  useEffect(() => {
+    if (to === from || amout === 0) {
+      setExchange(null)
+      return
+    }
+
+    const id = setTimeout(() => {
+      setExchange(null)
+      setLoading('Carregando...')
+      fetch(`https://economia.awesomeapi.com.br/last/${from}-${to}`)
+        .then(res => res.json())
+        .then(data => setExchange(data.USDBRL))
+        .catch(console.log)
+    }, 500)
+
+    return () => clearInterval(id)
+  }, [amout, from, to])
 
   const handleChangeFrom = e => setFrom(e.target.value)
   const handleChangeTo = e => setTo(e.target.value)
   const handleChangeAmout = e => setAmout(prev => prev === '' ? 0 : +e.target.value)
-
+  console.log(exchange)
   return (
     <>
       <input value={amout} type="number" autoFocus onChange={handleChangeAmout} />
@@ -34,7 +55,8 @@ const App = () => {
         </select>
       </div>
 
-      <h2>4.87 BRL</h2>
+      {loading && <h2>{loading}</h2>}
+      {exchange && <h2>{`${exchange.ask} ${to}`}</h2>}
     </>
   )
 }
